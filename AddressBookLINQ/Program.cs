bc47c0ee20ef78417742dbbbe672b33725520a8b
Akash <akashmane12598@gmail.com>
2020-11-07 01:29:12 +0530

    [Akash] Add. Created DataTable for AddressBook UC2

--- AddressBookLINQ/Program.cs
+++ AddressBookLINQ/Program.cs
@@ -7,6 +7,10 @@ namespace AddressBookLINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book LINQ!");
+
+            Console.WriteLine();
+            Console.WriteLine("UC2 Created DataTable for AddressBook");
+            Management.CreateTable_UC2();
         }
     }
 }
